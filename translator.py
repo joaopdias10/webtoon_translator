cf970d758b5c6b7002ac9178860f84151a39cefb
João Pedro <joaopedromeurerdias86@gmail.com>
2025-06-25 14:55:10 -0300

    PIL
    usando PIL para escrever

--- translator.py
+++ translator.py
@@ -4,56 +4,46 @@ import pytesseract
 import re
 from deep_translator import GoogleTranslator
 import textwrap
-#from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 model = YOLO("train/runs/detect/train8/weights/best.pt") #pega o meu modelo treinado
 #model = YOLO("scr_manga/weights/best.pt") #pega o modelo que achei na internet
-image = cv2.imread("scr_manga/inputs/img.jpeg") #carrega a imagem
+image = cv2.imread("scr_manga/inputs/img2.jpg") #carrega a imagem
 results = model(image) #passa a imagem pro modelo e recebe o resultado
-#cv2.imwrite("scr_manga/outputs/1.jpeg", results[0].plot()) #coloca a imagem resultante na pasta outputs
+cv2.imwrite("scr_manga/outputs/resultado.jpg", results[0].plot()) #coloca a imagem resultante na pasta outputs
 
-balloon = len(results[0].boxes) #numero de baloes de fala
+
+image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)) #converte do opencv para pil
+draw = ImageDraw.Draw(image_pil) #objeto de desenho
+fonte = ImageFont.truetype("arial.ttf", size=20)
+
+#balloon = len(results[0].boxes) #numero de baloes de fala
 
 for i,box in enumerate(results[0].boxes):
     x1, y1, x2, y2 = map(int, box.xyxy[0]) #coordenadas
 
     cropped = image[y1:y2, x1:x2] #recorta a imagem
-    config = r'--oem 3 --psm 6' #Vi em um video e melhorou o resultado kk
+    config = r'--oem 3 --psm 6' #vi em um video e melhorou o resultado kk
     text = pytesseract.image_to_string(cropped,config=config) #pega o texto da imagem
     text = re.sub(r'\s+', ' ', text).strip() #remove quebra de linha
-    text = GoogleTranslator(source='en', target='pt').translate(text)#traduz balão
-
+    text = GoogleTranslator(source='en', target='pt').translate(text) #traduz balão
+    text = text.capitalize() #formata o texto
     
-    
-    print(f"Balão {i+1}: {text}")
-    cv2.imwrite(f"scr_manga/outputs/recorte_{i}.jpg", cropped) #salva o balão
+    # Quando estava usando o OpenCV
+    #print(f"Balão {i+1}: {text}")
+    #cv2.imwrite(f"scr_manga/outputs/recorte_{i}.jpg", cropped) #salva o balão
+    #cv2.rectangle(image, (x1, y1), (x2, y2), (255, 255, 255), thickness=-1) #passa um "branco" no balão
 
-    cv2.rectangle(image, (x1, y1), (x2, y2), (255, 255, 255), thickness=-1) #Passa um "branco" no balão
+    draw.rectangle([x1, y1, x2, y2], fill=(255, 255, 255)) #passa o "branco"
 
+    caract = max(((x2 - x1) // 10), 1) #maximo num de caracteres, 10 pixels por caracter
+    linhas = textwrap.wrap(text, width=caract) #quebra o texto em linhas
 
+    for linha in linhas: #para cada linha escreve na imagem um com uma margem vertical e horizontal
+        draw.text((x1 + 5, y1 + 5), linha, font=fonte, fill=(0, 0, 0))
+        y1 += 25
 
-    largura_balao = x2 - x1
-    font_scale = 0.8
-    max_caracteres_por_linha = max((largura_balao // 10), 1)
-    linhas = textwrap.wrap(text, width=max_caracteres_por_linha)
-
-    # Desenha o texto no balão
-    y_texto = y1 + 20
-    for linha in linhas:
-        cv2.putText(
-            image, linha, (x1 + 5, y_texto),
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=font_scale,
-            color=(0, 0, 0),
-            thickness=1,
-            lineType=cv2.LINE_AA
-        )
-        y_texto += int(20 * font_scale)  # próxima linha
-
-
-
-
-cv2.imwrite("scr_manga/outputs/resultado.jpg", image) #salva as imagens com as alterações
+image_pil.save("scr_manga/outputs/resultado.jpg") #salva a imagem com as alterações
 
